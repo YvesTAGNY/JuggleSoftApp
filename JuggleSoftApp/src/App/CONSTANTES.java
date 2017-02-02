2b1c29469d57120939c843764b7a4b89177a151d
e154941 <e154941@E-0613.univ-lemans.fr>
2017-02-02 09:53:15 +0100

    creation de la classe constante et definition de quelques constantes

---
+++ JuggleSoftApp/src/App/CONSTANTES.java
@@ -0,0 +1,17 @@
+package App;
+
+public class CONSTANTES {
+
+	/**
+	 * declaration des constantes des couleurs que doivent prendre les balles
+	 * */
+	public static final int VERTE = 1;
+	public static final int ROUGE = 2;
+	public static final int BLEU = 3;
+	
+	/**
+	 * declaration des constantes identifiant les différents ports
+	 * */
+	public static final int COM = 0;
+	
+}
