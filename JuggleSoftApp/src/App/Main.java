21702700004eb73f479120c1a153f9eeb59b2022
e154941 <e154941@E-0613.univ-lemans.fr>
2017-02-02 10:25:39 +0100

    Implementation de la recupération d'un port ouvert et mise en ligne du scripte arduino_mpu6000

--- JuggleSoftApp/src/App/Main.java
+++ JuggleSoftApp/src/App/Main.java
@@ -1,10 +1,33 @@
 package App;
 
+import java.util.Enumeration;
+
+import gnu.io.CommPortIdentifier;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Program started");
+		
+		//déclaration d'un identifieur de ports
+	    CommPortIdentifier serialPortId;
+	    
+	    //static CommPortIdentifier sSerialPortId;
+	    Enumeration enumComm;
+	    
+	    //SerialPort serialPort;
 
+	    enumComm = CommPortIdentifier.getPortIdentifiers();
+	    while (enumComm.hasMoreElements()) {
+	     	serialPortId = (CommPortIdentifier) enumComm.nextElement();
+	     	if(serialPortId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+	    		System.out.println(serialPortId.getName());
+	    	}
+	    }
+
+		System.out.println("Finished successfully");
 	}
 
 }
